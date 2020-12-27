e76a4fbe5b3e904ffd2fec7fa142ce7c4e05e7d6
sameerasad <ssameerkhan525@gmail.com>
2020-12-27 21:30:08 +0500

    use realistic bd senario

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 function callback() {
   setTimeout(() => {
-    console.log("welcome to the async javasscript");
+    console.log("welcome to the async java script");
   }, 2000);
 }
 
 setTimeout(() => {
   console.log("this is the async javascript");
-}, 1000);
+}, 1500);
 
 callback();
+
 console.log("after");
+
+const getUser = (id) => {
+  setTimeout(() => {
+    console.log("reading from database");
+    return { id: id, githubusername: "sameer" };
+  }, 1000);
+};
+
+getUser(1);
