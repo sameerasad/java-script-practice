87eaac71c5afa0f3a92b3580b95b9c3c45a1cf4d
sameerasad <ssameerkhan525@gmail.com>
2021-01-04 12:43:37 +0500

    promise exercise code with async and await appraoch.

--- exercise.js
+++ exercise.js
@@ -1,27 +1,33 @@
-const getCustomer = (id, callback) => {
-  setTimeout(() => {
-    callback({
-      id: id,
-      name: "sameer asad",
-      isGold: true,
-      email: "ssameerkhan525@gmail.com",
-    });
-  }, 4000);
+const getCustomer = (id) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({
+        id: id,
+        name: "sameer asad",
+        isGold: true,
+        email: "ssameerkhan525@gmail.com",
+      });
+    }, 4000);
+  });
 };
 
-const getTopMovies = (callback) => {
-  setTimeout(() => {
-    callback(["movie1", "movie2"]);
-  }, 4000);
+const getTopMovies = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(["movie1", "movie2"]);
+    }, 4000);
+  });
 };
 
-const sendEmail = (email, movies, callback) => {
-  setTimeout(() => {
-    callback();
-  }, 4000);
+const sendEmail = (email, movies) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve();
+    }, 4000);
+  });
 };
-
-getCustomer(1, (customer) => {
+//callback approach
+/*getCustomer(1, (customer) => {
   console.log("customer", customer);
   if (customer.isGold) {
     getTopMovies((movies) => {
@@ -31,4 +37,21 @@ getCustomer(1, (customer) => {
       });
     });
   }
-});
+});*/
+
+//async and await approach
+async function notifyCustomer() {
+  try {
+    const customer = await getCustomer(3);
+    console.log("customers:", customer);
+    if (customer.isGold) {
+      const movies = await getTopMovies();
+      console.log(movies);
+      const emailSend = await sendEmail(customer.email, movies);
+      console.log("sent Email....");
+    }
+  } catch (err) {
+    console(new Error(err.message));
+  }
+}
+notifyCustomer();
